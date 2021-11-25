6f7db3f0e78beeff6b771a2a94f861ca34decd0e
SwampWalker <atonita@gmail.com>
2021-11-25 18:47:07 +0100

    Add some sophistication
    Some cli, write to a file, control number of samples.

--- src/bin/queue_counts.rs
+++ src/bin/queue_counts.rs
@@ -1,6 +1,27 @@
-use queues::queues::{Queue, QueueEvent};
+use std::io::{Error, Write, stdout};
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
+use structopt::StructOpt;
 
-fn main() {
+use queues::queues::{Queue, QueueEvent};
+use std::fs::File;
+
+#[derive(StructOpt)]
+struct Cli {
+    /// The number of samples to generate.
+    #[structopt(short, long, default_value = "1000000")]
+    samples: usize,
+    /// The path to the file to output to.
+    #[structopt(short, long, parse(from_os_str))]
+    path: Option<std::path::PathBuf>,
+}
+
+fn main() -> Result<(), Error> {
+    let terminate = Arc::new(AtomicBool::new(false));
+    signal_hook::flag::register(signal_hook::consts::SIGINT, Arc::clone(&terminate))?;
+
+    let args: Cli = Cli::from_args();
+
     let minute = 60.;
     let hour = 60. * minute;
     let customer_arrival_lambda = 5.8 / hour;
@@ -10,10 +31,26 @@ fn main() {
 
     let mut queue = Queue::new_exp_exp(customer_arrival_lambda, customer_service_lambda);
 
+    if let Some(path) = args.path {
+        let mut file = File::create(path).unwrap();
+        simulate(&mut file, terminate, args.samples, queue);
+    } else {
+        let stdout = stdout();
+        let mut stdout = stdout.lock();
+        simulate(&mut stdout, terminate, args.samples, queue);
+    }
+
+    Ok(())
+}
+
+fn simulate<OUT: Write>(out: &mut OUT, terminate: Arc<AtomicBool>, n_samples: usize, mut queue: Queue) {
+    let mut samples = 0;
     let mut a: u64 = 0;
     let mut d: u64 = 0;
-    println!("# time(s) arrivals departures in_system");
-    loop {
+    writeln!(out, "# time(s) arrivals departures in_system");
+    while !terminate.load(Ordering::Relaxed) && samples < n_samples {
+        samples += 1;
+
         let event = queue.next_event();
         let time = match event {
             QueueEvent::Arrival(t, _) => {
@@ -26,6 +63,6 @@ fn main() {
             }
         };
 
-        println!("{} {} {} {}", time, a, d, a - d);
+        writeln!(out, "{} {} {} {}", time, a, d, a - d);
     }
 }
